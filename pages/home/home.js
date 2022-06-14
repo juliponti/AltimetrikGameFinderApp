32d259c09c7ea82d16ddc6c179665d5b21d2860c
Julieta <julieta.pontino@gmail.com>
2022-06-14 13:34:48 -0300

    add one card view functionality

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,20 +3,32 @@ const layer = document.getElementById("layer");
 const cardContainer = document.getElementById("cards-container");
 const gallery = document.getElementById("gallery");
 const optionsContainer = document.getElementById("options-cont");
-const loader = document.querySelector("#loading");
+const loader = document.getElementById("loading");
 const cross = document.getElementById("cross");
 const userImg = document.getElementById("user-img-container");
+const threeCardVwBtn = document.getElementById("three-card-view-btn");
+const oneCardVwBtn = document.getElementById("one-card-view-btn");
+const oneViewContainer = document.getElementById("oneView-container");
 
 let counter = 0;
 let pageNum = 1;
 let currentValue;
 let searches = [];
+let bgDefault;
 
 if (localStorage.getItem("picture") == "true") {
   userImg.style.backgroundImage = `url("../../assets/desktop/home/header/Custom.png")`;
 } else {
   userImg.style.backgroundImage = `url("../../assets/desktop/home/header/EmptyState.png")`;
 }
+let threeViewVal;
+let oneViewVal;
+
+threeViewVal = true;
+oneViewVal = false;
+
+threeCardVwBtn.addEventListener("click", handleThreeView);
+oneCardVwBtn.addEventListener("click", handleOneView);
 
 gallery.addEventListener("scroll", handleScroll);
 input.addEventListener("keypress", debounce(handleChange, 350));
@@ -35,6 +47,11 @@ window.addEventListener("click", () => {
   optionsContainer.innerHTML = "";
 });
 
+function img(url) {
+  const iconUrl = url;
+  return `<img src="${iconUrl}" alt="" />`;
+}
+
 function displayLoading() {
   loader.classList.add("display");
 
@@ -47,10 +64,45 @@ function hideLoading() {
   loader.classList.remove("display");
 }
 
+function handleThreeView() {
+  const iconActive = img(
+    "../../assets/desktop/home/banner/three-view-active.svg"
+  );
+  const iconDisable = img(
+    "../../assets/desktop/home/banner/one-view-disable.svg"
+  );
+  threeCardVwBtn.innerHTML = iconActive;
+  oneCardVwBtn.innerHTML = iconDisable;
+
+  oneCardVwBtn.disabled = false;
+  threeCardVwBtn.disabled = true;
+  oneViewVal = !oneViewVal;
+  threeViewVal = !threeViewVal;
+
+  fetchData(+1);
+}
+
+function handleOneView() {
+  const iconActive = img(
+    "../../assets/desktop/home/banner/one-view-active.svg"
+  );
+  const iconDisable = img(
+    "../../assets/desktop/home/banner/three-view-disable.svg"
+  );
+  oneCardVwBtn.innerHTML = iconActive;
+  threeCardVwBtn.innerHTML = iconDisable;
+  oneCardVwBtn.disabled = true;
+  threeCardVwBtn.disabled = false;
+
+  oneViewVal = !oneViewVal;
+  threeViewVal = !threeViewVal;
+
+  fetchData(+1);
+}
+
 let card = (page) =>
   page.map((result) => {
     counter = counter + 1;
-
     let genreTitle = "";
     let consoles = [];
 
@@ -83,9 +135,11 @@ let card = (page) =>
       }
     }
 
-    return ` <div class="home__main__card">
+    const card1 = ` <div class="home__main__card">
             <div class="home__main__card-img__container">
-             <img src="${result.background_image}" alt="${result.name}"/>
+             <img src="${result.background_image || bgDefault}" alt="${
+      result.name
+    }"/>
               <svg
                 width="22"
                 height="21"
@@ -124,6 +178,61 @@ let card = (page) =>
               </div>
             </div>
           </div>`;
+
+    const card2 = `<div class="one-card-view__card">
+      <div class="one-card-view__card-img__container">
+      <img src="${result.background_image || bgDefault}" alt="${result.name}"/>
+        <svg
+          width="22"
+          height="21"
+          viewBox="0 0 22 21"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M6.33301 3.33342C4.47967 3.33342 2.99967 4.81742 2.99967 6.62275C2.99967 8.65475 4.17567 10.8228 5.99434 12.9468C7.52234 14.7294 9.37834 16.3374 10.9997 17.6348C12.621 16.3374 14.477 14.7281 16.005 12.9468C17.8237 10.8228 18.9997 8.65342 18.9997 6.62275C18.9997 4.81742 17.5197 3.33342 15.6663 3.33342C13.813 3.33342 12.333 4.81742 12.333 6.62275C12.333 6.97638 12.1925 7.31552 11.9425 7.56556C11.6924 7.81561 11.3533 7.95609 10.9997 7.95609C10.6461 7.95609 10.3069 7.81561 10.0569 7.56556C9.80682 7.31552 9.66634 6.97638 9.66634 6.62275C9.66634 4.81742 8.18634 3.33342 6.33301 3.33342ZM10.9997 2.87875C10.4351 2.18642 9.72327 1.62865 8.91602 1.24601C8.10876 0.863371 7.22636 0.665487 6.33301 0.666754C3.03167 0.666754 0.333008 3.32009 0.333008 6.62275C0.333008 9.62409 2.02234 12.4094 3.96901 14.6814C5.94367 16.9868 8.36501 18.9721 10.181 20.3854C10.4151 20.5675 10.7031 20.6663 10.9997 20.6663C11.2962 20.6663 11.5843 20.5675 11.8183 20.3854C13.6343 18.9721 16.0557 16.9854 18.0303 14.6814C19.977 12.4094 21.6663 9.62409 21.6663 6.62275C21.6663 3.32009 18.9677 0.666754 15.6663 0.666754C13.7863 0.666754 12.101 1.52809 10.9997 2.87875Z"
+            fill="white"
+          />
+        </svg>
+      </div>
+      <div class="one-card-view__card-info__container">
+        <div class="one-card-view__card-info-title__container">
+          <h4>${result.name}</h4>
+          <span>#${counter}</span>
+        </div>
+        <div class="one-card-view__card-info-details__container">
+          <div
+            class="one-card-view__card-info-details--text__container"
+          >
+            <div>
+              <p>Release date:</p>
+              <p>${result.released}</p>
+            </div>
+            <div>
+              <p>Genres:</p>
+              <p>${genreTitle.substring(0, genreTitle.length - 2)}</p>
+            </div>
+          </div>
+          <div class="one-card-view__card-console-logos__container">
+          ${consoles.join("")}
+          </div>
+        </div>
+        <div class="one-card-view__card-description__container">
+          <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, placeat quis? Maiores veritatis quidem tempore consectetur earum ut quaerat consequuntur aliquam et minima dolorem, sit eius nobis. Illum, vitae hic! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quis natus eaque iste molestiae nobis dolorem cumque fuga commodi distinctio perspiciatis, et iure, tenetur vero harum dolor necessitatibus odit voluptatibus.
+          </p>
+        </div>
+      </div>
+    </div>
+      `;
+
+    if (threeViewVal) {
+      return card1;
+    } else {
+      return card2;
+    }
   });
 
 let optionButton = (options) =>
@@ -145,7 +254,19 @@ async function fetchData(pageNum) {
   const currentCard = card(results);
   const allCards = currentCard.join(" ");
 
-  cardContainer.innerHTML += allCards;
+  if (threeViewVal) {
+    oneViewContainer.style.display = "none";
+    cardContainer.style.display = "grid";
+
+    cardContainer.innerHTML += allCards;
+    oneViewContainer.innerHTML = "";
+  } else {
+    oneViewContainer.style.display = "grid";
+    cardContainer.style.display = "none";
+
+    oneViewContainer.innerHTML += allCards;
+    cardContainer.innerHTML = "";
+  }
 }
 
 function handleScroll() {
